c8b0fc8865512152bcce99ae185157a91252f7f6
eduardfrigola <eduardfrigola@yahoo.com>
2016-07-29 17:00:44 +0200

    add elementOscilator

---
+++ src/elementOscilator.cpp
@@ -0,0 +1,9 @@
+//
+//  elementOscilator.cpp
+//  DGTL_Generator
+//
+//  Created by Eduard Frigola on 29/07/16.
+//
+//
+
+#include "elementOscilator.h"
---
+++ src/elementOscilator.h
@@ -0,0 +1,14 @@
+//
+//  elementOscilator.h
+//  DGTL_Generator
+//
+//  Created by Eduard Frigola on 29/07/16.
+//
+//
+
+#ifndef elementOscilator_h
+#define elementOscilator_h
+
+#include <stdio.h>
+
+#endif /* elementOscilator_h */
